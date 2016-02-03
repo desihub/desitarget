85882eb856dda318b2d7833fa5f8cab84afcde1b
Kaylan Burleigh <kburleigh@lbl.gov>
2016-02-02 16:04:36 -0800

    write schema files AND correctly loads the data to psql db, currently does so for tables: bricks, cfhtls_d2_r

--- db/schema_generator.py
+++ db/schema_generator.py
@@ -1,10 +1,19 @@
 import thesis_code.fits
 from astropy.io import fits
-import argparse
+from argparse import ArgumentParser
 import numpy as np
+import os
+import psycopg2
+import sys
+
+def rem_if_exists(name):
+    if os.path.exists(name):
+        if os.system(' '.join(['rm','%s' % name]) ): raise ValueError
 
 def write_schema(table,keys,sql_dtype,addrows=[],indexing=[]):
-    fin=open(table+'.table','w')
+    outname= table+'.table'
+    rem_if_exists(outname)
+    fin=open(outname,'w')
     fin.write('CREATE SEQUENCE %s_id_seq;' % table)
     fin.write('\n\n'+'CREATE TABLE %s (' % table)
     #add indexing names
@@ -20,26 +29,33 @@ def write_schema(table,keys,sql_dtype,addrows=[],indexing=[]):
         if index == indexing[-1]: fin.write('\n')
     fin.close()
 
-def insert_query(ith_row,data,keys,args):
-    query = 'INSERT INTO %s ( ' % args.table    
+def insert_query(table,ith_row,data,keys,returning=False,newkeys=[],newvals=[]):
+    query = 'INSERT INTO %s ( ' % table    
+    for nk in newkeys: query+= '%s, ' % nk
     for key in keys:
-        query+= key
+        query+= key.lower()
         if key != keys[-1]: query+= ', '
     query+= ' ) VALUES ( '
+    for nv in newvals: query+= '%s, ' % str(nv)
     for key in keys:
-        query+= '%s' % str(data[key][i])
+        if np.issubdtype(data[key][i].dtype, str): query+= "'%s'" % data[key][i]
+        else: query+= "%s" % str(data[key][i])
         if key != keys[-1]: query+= ', '
-    query+= ' )' #RETURNING id'
+    if returning: query+= ' ) RETURNING id'
+    else: query+= ' )'
     return query
 
 
-parser = argparse.ArgumentParser(description="test")
+parser = ArgumentParser(description="test")
 parser.add_argument("-fits_file",action="store",help='',required=True)
 parser.add_argument("-table",choices=['bricks','cfhtls_d2_r'],action="store",help='',required=True)
+parser.add_argument("-overw_schema",action="store",help='set to anything to write schema to file, overwritting the previous file',required=False)
+parser.add_argument("-load_db",action="store",help='set to anything to load and write to db',required=False)
 args = parser.parse_args()
 
 a=fits.open(args.fits_file)
 data,keys= thesis_code.fits.getdata(a,1)
+nrows = data.shape[0] 
 
 #write schemas
 if args.table == 'bricks':
@@ -50,11 +66,24 @@ if args.table == 'bricks':
         elif np.issubdtype(data[key].dtype, int): sql_dtype[key]= 'integer'
         elif np.issubdtype(data[key].dtype, float): sql_dtype[key]= 'real'
         else: raise ValueError
-    more_rows= ["id bigint default null"]
+    #schema
+    more_rows= ["id bigint primary key not null default nextval('%s_id_seq'::regclass)" % args.table]
     indexes= ["CREATE INDEX %s_q3c_idx ON %s (q3c_ang2ipix(ra,dec))" % (args.table,args.table),\
                 "CLUSTER %s_q3c_idx on %s" % (args.table,args.table),\
                 "CREATE INDEX %s_brickid_idx ON %s (brickid)" %(args.table,args.table)]
-    write_schema(args.table,keys,sql_dtype,addrows=more_rows,indexing=indexes)
+    if args.overw_schema:
+        write_schema(args.table,keys,sql_dtype,addrows=more_rows,indexing=indexes)
+    #db
+    con = psycopg2.connect(host='scidb2.nersc.gov', user='desi_admin', database='desi')
+    cursor = con.cursor()
+    for i in range(0,30): #nrows):
+        print 'row= ',i
+        query= insert_query(args.table,i,data,keys)
+        if args.load_db: cursor.execute(query) 
+    if args.load_db: 
+        con.commit()
+        print 'finished bricks load'
+    print 'query looks like this: \n',query    
 
 elif args.table == 'cfhtls_d2_r':
     obj_keys,fluxes_keys=[],[]
@@ -70,15 +99,32 @@ elif args.table == 'cfhtls_d2_r':
         elif np.issubdtype(data[key].dtype, int): sql_dtype[key]= 'integer'
         elif np.issubdtype(data[key].dtype, float): sql_dtype[key]= 'real'
         else: raise ValueError
-
-    #indexing names for schemas
-    more_obj_rows= ["id bigint default null",\
+    #schema
+    more_obj_rows= ["id bigint primary key not null default nextval('%s_id_seq'::regclass)" % (args.table+'_objs'),\
                     "brickid integer default null"]#primary key not null default nextval('%s_id_seq'::regclass)," % args.table)
-    more_flux_rows= ["id bigint default null",\
-                    "cand_id bigint default null"] #REFERENCES candidate (id)
-    write_schema(args.table+'_objs',obj_keys,sql_dtype,addrows=more_obj_rows)
-    write_schema(args.table+'_flux',fluxes_keys,sql_dtype,addrows=more_flux_rows)
-
+    more_flux_rows= ["id bigint primary key not null default nextval('%s_id_seq'::regclass)" % (args.table+'_flux'),\
+                    "cand_id bigint REFERENCES %s (id)" % (args.table+'_objs')]
+    if args.overw_schema:
+        write_schema(args.table+'_objs',obj_keys,sql_dtype,addrows=more_obj_rows)
+        write_schema(args.table+'_flux',fluxes_keys,sql_dtype,addrows=more_flux_rows)
+    #db
+    con = psycopg2.connect(host='scidb2.nersc.gov', user='desi_admin', database='desi')
+    cursor = con.cursor()
+    for i in range(0, 30): #nrows):
+        print 'row= ',i
+        query1= insert_query(args.table+'_objs',i,data,obj_keys,returning=True)
+        if args.load_db: 
+            cursor.execute(query1) 
+            id = cursor.fetchone()[0]
+        else: id=2 #junk val so can print what query would look like 
+        query2= insert_query(args.table+'_flux',i,data,fluxes_keys,newkeys=['cand_id'],newvals=[id])
+        if args.load_db: cursor.execute(query2) 
+    if args.load_db: 
+        con.commit()
+        print 'finished cfhtls_d2_r load'
+    print 'cfhtls_d2_r query looks like this: \n'    
+    print 'obj query: \n',query1    
+    print 'flux query: \n',query2    
 else: raise ValueError
 #output schemas
 #print 'writing objects table'
@@ -86,14 +132,8 @@ else: raise ValueError
 
 #load data into obj and fluxes tables in db
 # Fire up the db
-#con = psycopg2.connect(host='scidb2.nersc.gov', user='desi_admin', database='desi')
-#cursor = con.cursor()
 #cursor.execute( "UPDATE bricks set filename = %s, loaded = 'true', tractorvr = %s where brickid = %s", (fimage, tractver, hdrbrickid,)  )
 
-nrows = data.shape[0] 
-for i in range(0, 30): #nrows):
-    query= insert_query(i,data,keys,args)    
-    #cursor.execute(query) 
      
 #   id = cursor.fetchone()[0]
 ###
