3408de0726868e78756c52a04092dea7b8cabf1a
Stephen Bailey <stephen.code@gmail.com>
2016-02-14 14:20:56 -0800

    add tests of individual cuts

--- py/desitarget/test/test_cuts.py
+++ py/desitarget/test/test_cuts.py
@@ -31,7 +31,7 @@ class TestCuts(unittest.TestCase):
             self.assertIn(col, t2.dtype.names)
             self.assertTrue(np.all(t1[col] == t2[col]))
 
-    def test_cuts1(self):
+    def test_cuts_basic(self):
         #- Cuts work with either data or filenames
         desi, bgs, mws = cuts.apply_cuts(self.tractorfiles[0])
         desi, bgs, mws = cuts.apply_cuts(self.sweepfiles[0])
@@ -43,6 +43,43 @@ class TestCuts(unittest.TestCase):
         # bgs_any1 = (desi & desi_mask.BGS_ANY)
         # bgs_any2 = (bgs != 0)
         # self.assertTrue(np.all(bgs_any1 == bgs_any2))
+
+    def test_cuts_noprimary(self):
+        #- cuts should work with or without "primary"
+        targets = Table.read(self.sweepfiles[0])
+        desi1, bgs1, mws1 = cuts.apply_cuts(targets)
+        targets.remove_column('BRICK_PRIMARY')
+        desi2, bgs2, mws2 = cuts.apply_cuts(targets)
+        self.assertTrue(np.all(desi1==desi2))
+        self.assertTrue(np.all(bgs1==bgs2))
+        self.assertTrue(np.all(mws1==mws2))
+
+    def test_single_cuts(self):
+        #- test cuts of individual target classes
+        targets = Table.read(self.sweepfiles[0])
+        flux = cuts.unextinct_fluxes(targets)
+        gflux = flux['GFLUX']
+        rflux = flux['RFLUX']
+        zflux = flux['ZFLUX']
+        w1flux = flux['W1FLUX']
+        wflux = flux['WFLUX']
+        primary = targets['BRICK_PRIMARY']
+        lrg1 = cuts.isLRG(rflux, zflux, w1flux, primary=None)
+        lrg2 = cuts.isLRG(rflux, zflux, w1flux, primary=primary)
+        self.assertTrue(np.all(lrg1==lrg2))
+
+        elg1 = cuts.isELG(gflux, rflux, zflux, primary=primary)
+        elg2 = cuts.isELG(gflux, rflux, zflux, primary=None)
+        self.assertTrue(np.all(elg1==elg2))
+
+        psftype = targets['TYPE']
+        bgs1 = cuts.isBGS(rflux, type=psftype, primary=primary)
+        bgs2 = cuts.isBGS(rflux, type=None, primary=None)
+        self.assertTrue(np.all(bgs1==bgs2))
+
+        qso1 = cuts.isQSO(gflux, rflux, zflux, wflux, type=psftype, primary=primary)
+        qso2 = cuts.isQSO(gflux, rflux, zflux, wflux, type=None, primary=None)
+        self.assertTrue(np.all(qso1==qso2))
 
     #- cuts should work with tables from several I/O libraries
     def _test_table_row(self, targets):
