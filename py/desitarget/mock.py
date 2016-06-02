bdea3d2da211d18232b8ab12c6aa40d18c4b17a0
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-06-02 08:41:55 -0700

    more documentation in mock functions + use of kwargs

--- py/desitarget/mock.py
+++ py/desitarget/mock.py
@@ -38,6 +38,27 @@ def estimate_density(ra, dec):
     return density
 
 def reduce(ra, dec, z, frac):
+    """Reduces the size of input RA, DEC, Z arrays.
+    
+    Args:
+        ra: array_like
+            An array with RA positions.
+        dec: array_like
+            An array with Dec positions.
+        z: array_like
+            An array with redshifts
+        frac: float
+           Fraction of input arrays to be kept.
+
+    Returns:
+        ra_kept: array_like
+             Subset of input RA.
+        dec_kept: array_like
+             Subset of input Dec.
+        z_kept: array_like
+             Subset of input Z.
+
+    """
     xra = np.array(ra)
     xdec = np.array(dec)
     xzz = np.array(z)
@@ -54,14 +75,49 @@ def reduce(ra, dec, z, frac):
     return((yra,ydec,yzz))
 
 
-def select_population(ra, dec, z, goal_density=0.0, min_z=-1.0, max_z=1000.0, true_type='GALAXY', 
-                      desi_target_flag=0, bgs_target_flag=0, mws_target_flag=0):
-    ii = ((z>=min_z) & (z<=max_z))
+def select_population(ra, dec, z, **kwargs):
+
+    """Selects points in RA, Dec, Z to assign them a target population.
+    
+    Args:
+        ra: array_like
+            An array with RA positions.
+        dec: array_like
+            An array with Dec positions.
+        z: array_like
+            An array with redshifts
+
+
+    **kwargs:
+        goal_density: float
+            Number density (n/deg^2) desired for this set of points.
+goal_density=0.0, 
+min_z=-1.0, max_z=1000.0, true_type='UNKNOWN', 
+                      desi_target_flag=0, bgs_target_flag=0, mws_target_flag=0):            
+
+    Returns:
+        ra_pop: array_like (float)
+             Subset of input RA.
+        dec_pop: array_like (float)
+             Subset of input Dec.
+        z_pop: array_like (float)
+             Subset of input Z.
+        desi_target_pop: array_like (int)
+             Array of DESI target flags corresponding to the input desi_target_flag
+        bgs_target_pop: array_like (int)
+             Array of BGS target flags corresponding to the input bgs_target_flag
+        mws_target_pop: array_like (int)
+             Array of MWS target flags corresponding to the input mws_target_flag
+        true_type_pop: array_like (string)
+             Array of true types corresponding to the input true_type.
+    """
+
+    ii = ((z>=kwargs['min_z']) & (z<=kwargs['max_z']))
 
     mock_dens = estimate_density(ra[ii], dec[ii])
-    frac_keep = min(goal_density/mock_dens , 1.0)
-    if mock_dens < goal_density:
-        print("WARNING: mock cannot achieve the goal density. Goal {}. Mock {}".format(goal_density, mock_dens))
+    frac_keep = min(kwargs['goal_density']/mock_dens , 1.0)
+    if mock_dens < kwargs['goal_density']:
+        print("WARNING: mock cannot achieve the goal density. Goal {}. Mock {}".format(kwargs['goal_density'], mock_dens))
 
 
     ra_pop, dec_pop, z_pop = reduce(ra[ii], dec[ii], z[ii], frac_keep)
@@ -70,11 +126,10 @@ def select_population(ra, dec, z, goal_density=0.0, min_z=-1.0, max_z=1000.0, tr
 
     print("keeping total={} fraction={}".format(n, frac_keep))
 
-    desi_target_pop  = np.zeros(n, dtype='i8'); desi_target_pop[:] = desi_target_flag
-    bgs_target_pop = np.zeros(n, dtype='i8'); bgs_target_pop[:] = bgs_target_flag
-    mws_target_pop = np.zeros(n, dtype='i8'); mws_target_pop[:] = mws_target_flag
-    true_type_pop = np.zeros(n, dtype='S10'); true_type_pop[:] = true_type
-    sub_prior_pop = np.random.uniform(0, 1, size=n)
+    desi_target_pop  = np.zeros(n, dtype='i8'); desi_target_pop[:] = kwargs['desi_target_flag']
+    bgs_target_pop = np.zeros(n, dtype='i8'); bgs_target_pop[:] = kwargs['bgs_target_flag']
+    mws_target_pop = np.zeros(n, dtype='i8'); mws_target_pop[:] = kwargs['mws_target_flag']
+    true_type_pop = np.zeros(n, dtype='S10'); true_type_pop[:] = kwargs['true_type']
 
-    return ((ra_pop, dec_pop, z_pop, desi_target_pop, bgs_target_pop, mws_target_pop, true_type_pop, sub_prior_pop))
+    return ((ra_pop, dec_pop, z_pop, desi_target_pop, bgs_target_pop, mws_target_pop, true_type_pop))
 
