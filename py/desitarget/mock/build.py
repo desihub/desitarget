7ad333b975840dff106be5b892baa769e8b68f7a
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2016-10-13 16:13:14 -0700

    writes Targest and Truth compiling all available mocks

--- py/desitarget/mock/build.py
+++ py/desitarget/mock/build.py
@@ -16,6 +16,8 @@ from   desitarget.targetmask import desi_mask
 from   desitarget.mock.io    import decode_rownum_filenum
 import desitarget.mock.io as mockio
 import desitarget.mock.selection as mockselect
+from desitarget import obsconditions
+import desispec.brick
 
 ############################################################
 def targets_truth(params):
@@ -47,7 +49,6 @@ def targets_truth(params):
 
     # load all the mocks
     for source_name in sorted(source_defs.keys()):
-
         source_format = params['sources'][source_name]['format']
         source_path = params['sources'][source_name]['root_mock_dir']
 
@@ -58,7 +59,7 @@ def targets_truth(params):
 
     print('loaded {} mock sources'.format(len(source_data_all)))
 
-    print('Making target selection for')
+    print('Making target selection')
     # runs target selection on every mock
     for source_name in sorted(source_defs.keys()):
         source_selection = params['sources'][source_name]['selection']
@@ -69,31 +70,99 @@ def targets_truth(params):
         selection_function = source_selection + '_select'
         result = getattr(mockselect, selection_function.lower())(source_data, source_name, **source_dict)
         target_mask_all[source_name] = result
-
-    # writes targets and truth to disk
-
-
-
-#    for source_name in sorted(source_defs.keys()):
-#        module_name = 'desitarget.mock.{}'.format(source_name)
-#        print('')
-#        print('Reading mock {}'.format(source_name))
-#        print('Using module {}'.format(module_name))
-
-#        M = importlib.import_module(module_name)
-#        t0 = time.time()
-
-#        mock_kwargs                          = copy(source_defs[source_name])
-#        mock_kwargs['output_dir']            = output_dir
-#        mock_kwargs['write_cached_targets']  = True
-#        mock_kwargs['remake_cached_targets'] = reset
-
-#        targets, truth, sourcefiles = M.build_mock_target(**mock_kwargs)
-#        t1 = time.time()
-
-#        targets_all.append(targets)
-#        truth_all.append(truth)
-#        sourcefiles_all.append(sourcefiles)
-#        print('Data read for mock {}, took {:f}s'.format(source_name,t1-t0))
+        
+    # consolidates all relevant arrays across mocks
+    ra_total = np.empty(0)
+    dec_total = np.empty(0)
+    z_total = np.empty(0)
+    desi_target_total = np.empty(0, dtype='i8')
+    bgs_target_total = np.empty(0, dtype='i8')
+    mws_target_total = np.empty(0, dtype='i8')
+    true_type_total = np.empty(0, dtype='S10')
+    true_subtype_total = np.empty(0, dtype='S10')
+    obsconditions_total = np.empty(0, dtype='uint16')
 
 
+    print('Collects information across mock files')
+    for source_name in sorted(source_defs.keys()):
+        source_data = source_data_all[source_name]
+        target_mask = target_mask_all[source_name]
+
+        ii = target_mask >-1 #only targets that passed cuts
+
+        #define all flags
+        desi_target = 0 * target_mask[ii]
+        bgs_target = 0 * target_mask[ii] 
+        mws_target = 0 * target_mask[ii]
+        if source_name in ['ELG', 'LRG', 'QSO']:
+            desi_target = target_mask[ii]
+        if source_name in ['BGS']:
+            bgs_target = target_mask[ii]
+        if source_name in ['MWS_MAIN', 'MWS_WD']:
+            mws_target = target_mask[ii]
+
+
+        # define names that go into Truth
+        n = len(source_data['RA'][ii])
+        true_type = np.zeros(n, dtype='S10'); true_type[:] = source_name
+        true_subtype = np.zeros(n, dtype='S10'); true_subtype[:] = source_name
+
+        #define obsconditions
+        source_obsconditions = np.ones(n,dtype='uint16')
+        if source_name in ['ELG', 'LRG', 'QSO']:
+            source_obsconditions[:] = obsconditions.DARK
+        if source_name in ['BGS']:
+            source_obsconditions[:] = obsconditions.BRIGHT
+        if source_name in ['MWS_MAIN', 'MWS_WD']:
+            source_obsconditions[:] = obsconditions.BRIGHT
+
+        #append to the arrays that will go into Targets
+        ra_total = np.append(ra_total, source_data['RA'][ii])
+        dec_total = np.append(dec_total, source_data['DEC'][ii])
+        z_total = np.append(z_total, source_data['Z'][ii])
+        desi_target_total = np.append(desi_target_total, desi_target)
+        bgs_target_total = np.append(bgs_target_total, bgs_target)
+        mws_target_total = np.append(mws_target_total, mws_target)
+        true_type_total = np.append(true_type_total, true_type)
+        true_subtype_total = np.append(true_subtype_total, true_subtype)
+        obsconditions_total = np.append(obsconditions_total, source_obsconditions)
+
+        print('sub', len(source_data['RA'][ii]), len(source_data['RA']))
+
+    # create unique IDs, subpriorities and bricknames across all mock files
+    n = len(ra_total)
+    print('Great total of {} targets'.format(n))
+    targetid = np.random.randint(2**62, size=n)
+    subprior = np.random.uniform(0., 1., size=n)
+    brickname = desispec.brick.brickname(ra_total, dec_total)
+
+
+    # write the Targets to disk
+    print('Started writing Targets file')
+    targets_filename = os.path.join(params['output_dir'], 'targets.fits')
+    targets = Table()
+    targets['TARGETID'] = targetid
+    targets['BRICKNAME'] = brickname
+    targets['RA'] = ra_total
+    targets['DEC'] = dec_total
+    targets['DESI_TARGET'] = desi_target_total
+    targets['BGS_TARGET'] = bgs_target_total
+    targets['MWS_TARGET'] = mws_target_total
+    targets['SUBPRIORITY'] = subprior
+    targets['OBSCONDITIONS'] = obsconditions_total
+    targets.write(targets_filename, overwrite=True)
+    print('Finished writing Targets file')
+
+    # write the Truth to disk
+    print('Started writing Truth file')
+    truth_filename = os.path.join(params['output_dir'], 'truth.fits')
+    truth = Table()
+    truth['TARGETID'] = targetid
+    truth['BRICKNAME'] = brickname
+    truth['RA'] = ra_total
+    truth['DEC'] = dec_total
+    truth['TRUEZ'] = z_total
+    truth['TRUETYPE'] = true_type_total
+    truth['TRUESUBTYPE'] = true_subtype_total
+    truth.write(truth_filename, overwrite=True)
+    print('Finished writing Truth file')
---
+++ py/desitarget/mock/selection.py
@@ -0,0 +1,330 @@
+# Licensed under a 4-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+===========================
+desitarget.mock.mws_galaxia
+===========================
+
+Builds target/truth files from already existing mock data
+"""
+from __future__ import (absolute_import, division)
+#
+import numpy as np
+import os, re
+import desitarget.mock.io 
+import desitarget.io
+from   desitarget import mws_mask, desi_mask, bgs_mask
+import os
+from   astropy.table import Table, Column
+import fitsio
+
+
+    
+############################################################
+def mag_select(data, source_name, **kwargs):
+    """
+    Apply the selection function to determine the target class of each entry in
+    the input catalog.
+
+    Parameters:
+    -----------
+        mws_data: dict
+            Data required for selection
+        kwargs: dict
+            Data required to make a sample selection. 
+            Should include:
+            mag_faintest:    float
+                Hard faint limit for inclusion in survey.
+            mag_faint_filler:  float
+                Magintude fainter than which stars are considered filler, rather
+                than part of the main sample.
+            mag_bright:      float 
+                Hard bright limit for inclusion in survey.
+    """
+
+    target_class = -1
+
+    if(source_name == 'MWS_MAIN'):
+        mag_bright = kwargs['mag_bright']
+        mag_faintest = kwargs['mag_faintest']
+        mag_faint_filler = kwargs['mag_faint_filler']
+
+
+        # Parameters
+        SELECTION_MAG_NAME = 'SDSSr_obs'
+
+        # Will populate this array with the bitmask values of each target class
+        target_class = np.zeros(len(data[SELECTION_MAG_NAME]),dtype=np.int64) - 1 
+                
+        fainter_than_bright_limit  = data[SELECTION_MAG_NAME]  >= mag_bright
+        fainter_than_filler_limit  = data[SELECTION_MAG_NAME]  >= mag_faint_filler
+        brighter_than_filler_limit = data[SELECTION_MAG_NAME]  <  mag_faint_filler
+        brighter_than_faint_limit  = data[SELECTION_MAG_NAME]  <  mag_faintest
+
+        # Main sample
+        select_main_sample               = (fainter_than_bright_limit) & (brighter_than_filler_limit)    
+        target_class[select_main_sample] = mws_mask.mask('MWS_MAIN')
+                
+        # Faint sample
+        select_faint_filler_sample               = (fainter_than_filler_limit) & (brighter_than_faint_limit)    
+        target_class[select_faint_filler_sample] = mws_mask.mask('MWS_MAIN_VERY_FAINT')
+        
+    if(source_name == 'MWS_WD'):
+        mag_bright = kwargs['mag_bright']
+        mag_faint = kwargs['mag_faint']
+        # Parameters
+        SELECTION_MAG_NAME = 'magg'
+        
+        # Will populate this array with the bitmask values of each target class
+        target_class = np.zeros(len(data[SELECTION_MAG_NAME]),dtype=np.int64) - 1
+        
+        fainter_than_bright_limit  = data[SELECTION_MAG_NAME]  >= mag_bright
+        brighter_than_faint_limit  = data[SELECTION_MAG_NAME]  <  mag_faint
+        is_wd                      = data['WD'] == 1
+        
+        # WD sample
+        select_wd_sample               = (fainter_than_bright_limit) & (brighter_than_faint_limit) & (is_wd)   
+        target_class[select_wd_sample] = mws_mask.mask('MWS_WD')
+        
+        
+        # Nearby ('100pc') sample -- everything in the input table that isn't a WD
+        # Expect to refine this in future
+        select_nearby_sample               = (fainter_than_bright_limit) & (brighter_than_faint_limit) & (np.invert(is_wd))
+        target_class[select_nearby_sample] = mws_mask.mask('MWS_NEARBY')
+
+
+    if(source_name == 'BGS'):
+        mag_bright = kwargs['mag_bright']
+        mag_faintest = kwargs['mag_faintest']
+        mag_priority_split = kwargs['mag_priority_split']
+
+        # Parameters
+        SELECTION_MAG_NAME = 'SDSSr_true'
+        
+        # Will populate this array with the bitmask values of each target class
+        target_class = np.zeros(len(data[SELECTION_MAG_NAME]),dtype=np.int64) - 1
+        
+        fainter_than_bright_limit  = data[SELECTION_MAG_NAME]  >= mag_bright
+        brighter_than_split_mag    = data[SELECTION_MAG_NAME]   < mag_priority_split
+        fainter_than_split_mag     = data[SELECTION_MAG_NAME]  >= mag_priority_split
+        brighter_than_faint_limit  = data[SELECTION_MAG_NAME]   < mag_faintest
+
+        # Bright sample
+        select_bright_sample               = (fainter_than_bright_limit) & (brighter_than_split_mag)
+        target_class[select_bright_sample] = bgs_mask.mask('BGS_BRIGHT')
+
+        # Fxsaint sample
+        select_faint_sample               = (fainter_than_split_mag) & (brighter_than_faint_limit)
+        target_class[select_faint_sample] = bgs_mask.mask('BGS_FAINT')
+
+
+    return target_class
+
+############################################################
+def build_mock_target(root_mock_dir='', output_dir='',
+                      mock_ext='hdf5',
+                      targets_name='bgs_durahm_mxxl_targets.fits',
+                      truth_name='bgs_durahm_mxxl_truth.fits',
+                      selection_name='bgs_durahm_mxxl_selection.fits',
+                      mag_faintest=20.0, mag_priority_split=19.5, mag_bright=15.0, 
+                      remake_cached_targets=False,
+                      write_cached_targets=True,
+                      rand_seed=42):
+
+    """Builds a Target and Truth files from a series of mock files.
+
+    Parameters:
+    ----------
+        rand_seed: int
+            seed for random number generator.
+    """
+    desitarget.io.check_fitsio_version()
+    np.random.seed(seed=rand_seed)
+
+    targets_filename = os.path.join(output_dir, targets_name)
+    truth_filename   = os.path.join(output_dir, truth_name)
+
+    target_exists    = os.path.exists(targets_filename)
+    truth_exists     = os.path.exists(truth_filename)
+
+    # Do we need to store copies of the targets and truth on disk?
+    write_new_files = (remake_cached_targets) or not (target_exists and truth_exists)
+
+    if not write_new_files:
+        # Report the size of the existing input files
+        targets_filesize = os.path.getsize(targets_filename) / (1024.0**3)
+        truth_filesize   = os.path.getsize(targets_filename) / (1024.0**3)
+
+        # Just read from the files we already have. Need to convert to astropy
+        # table for consistency of return types.
+        print("Reading existing files:")
+        print("    Targets: {} ({:4.3f} Gb)".format(targets_filename,targets_filesize))
+        targets   = Table(desitarget.io.whitespace_fits_read(targets_filename,ext='TARGETS'))
+        print("    Truth:   {} ({:4.3f} Gb)".format(truth_filename,truth_filesize))
+        truth     = Table(desitarget.io.whitespace_fits_read(truth_filename,ext='TRUTH'))
+        file_list = Table(desitarget.io.whitespace_fits_read(truth_filename,ext='SOURCES'))
+    else:
+        # Read the mocks on disk. This returns a dict.
+        # FIXME should just use table here too?
+        data, file_list = desitarget.mock.io.read_mock_bgs_mxxl_brighttime(root_mock_dir=root_mock_dir,mock_ext=mock_ext)
+        data_keys       = list(data.keys())
+        n               = len(data[data_keys[0]])
+        
+        # Allocate target classes and priorities
+        target_class = bgs_selection(data,
+                                     mag_faintest       = mag_faintest,
+                                     mag_priority_split = mag_priority_split,
+                                     mag_bright         = mag_bright)
+        # Identify all targets
+        in_target_list = target_class >= 0
+        ii             = np.where(in_target_list)[0]
+        n_selected     = len(ii)
+
+        print("Properties read from mock: {}".format(data.keys()))
+        print("n_items in full catalog: {}".format(n))
+
+        print('Selection criteria:')
+        for criterion in ['mag_faintest','mag_priority_split','mag_bright']:
+            print(" -- {:30s} {}".format(criterion,locals().get(criterion)))
+
+        print("n_items after selection: {}".format(n_selected))
+            
+        # targetid  = np.random.randint(2**62, size=n_selected)
+
+        # Targetids are row numbers in original input
+        #targetid  = np.arange(0,n)[ii]
+
+        # Targetids are brick/object combos from original input
+        #BRICKID_FACTOR           = 1e10 # Max 10^10 objects per brick
+        #combined_brick_object_id = data['brickid'][ii]*BRICKID_FACTOR + data['objid'][ii]
+        #targetid                 = np.asarray(combined_brick_object_id,dtype=np.int64)
+
+        # This routine assigns targetIDs that encode the mapping of each row in the
+        # target outputfile to a filenumber and row in the set of mock input files.
+        # This targetID will be further modified when all target lists are merged.
+        targetid = desitarget.mock.io.encode_rownum_filenum(data['rownum'][ii],data['filenum'][ii])
+
+        # Assign random subpriorities for now
+        subprior  = np.random.uniform(0., 1., size=n_selected)
+
+        # Assign DESI-standard bricknames
+        # CAREFUL: These are not the bricknames used by the input catalog!
+        brickname = desispec.brick.brickname(data['RA'][ii], data['DEC'][ii])
+
+        # assign target flags and true types
+        desi_target_pop   = np.zeros(n_selected, dtype='i8')
+        bgs_target_pop    = np.zeros(n_selected, dtype='i8') 
+        mws_target_pop    = np.zeros(n_selected, dtype='i8') 
+        bgs_target_pop[:] = target_class[ii]
+
+        # APC This is a string? 
+        # FIXME (APC) This looks totally wrong, especially if the target class
+        # encodes a combination of bits such that mask.names() returns a list.
+        # The 'true type' should be something totally separate (an LRG is an
+        # LRG, regardless of whether it's in the North or South, etc.).
+        true_type_pop     = np.asarray(desitarget.targets.target_bitmask_to_string(target_class[ii],bgs_mask),dtype='S10')
+
+        # Write the Targets to disk
+        targets = Table()
+        targets['TARGETID']    = targetid
+        targets['BRICKNAME']   = brickname
+        targets['RA']          = data['RA'][ii]
+        targets['DEC']         = data['DEC'][ii]
+        targets['DESI_TARGET'] = desi_target_pop
+        targets['BGS_TARGET']  = bgs_target_pop
+        targets['MWS_TARGET']  = mws_target_pop
+        targets['SUBPRIORITY'] = subprior
+
+        # Write the Truth to disk
+        truth = Table()
+        truth['TARGETID']  = targetid
+        truth['BRICKNAME'] = brickname
+        truth['RA']        = data['RA'][ii]
+        truth['DEC']       = data['DEC'][ii]
+        truth['TRUEZ']     = data['Z'][ii]
+
+        # True type is just targeted type for now.
+        truth['TRUETYPE']  = true_type_pop
+
+        # File list
+        file_list = np.array(file_list,dtype=[('FILE','|S500'),('NROWS','i8')])
+        assert(np.all([len(x[0]) < 500 for x in file_list]))
+
+        # Write Targets
+        if write_cached_targets:
+            print('Writing target list and truth for this mock...')
+            with fitsio.FITS(targets_filename,'rw',clobber=True) as fits:
+                fits.write(desiutil.io.encode_table(targets).as_array(),extname='TARGETS',clobber=True)
+
+            # Write truth, slightly convoluted because we write two tables
+            with fitsio.FITS(truth_filename,'rw',clobber=True) as fits:
+                fits.write(desiutil.io.encode_table(truth).as_array(), extname='TRUTH')
+                fits.write(file_list, extname='SOURCES')                
+ 
+            print("Wrote new files:")
+            print("    Targets: {}".format(targets_filename))
+            print("    Truth:   {}".format(truth_filename))
+
+    return targets, truth, Table(file_list)
+    
+
+    return target_class
+
+
+
+def estimate_density(ra, dec):
+    """Estimate the number density from a small patch
+    
+    Args:
+        ra: array_like
+            An array with RA positions.
+        dec: array_like
+            An array with Dec positions.
+
+    Returns:
+        density: float
+           Object number density computed over a small patch.
+    """
+    density = 0.0 
+
+    footprint_area = 20. * 35.* np.sin(35. * np.pi/180.)/(35. * np.pi/180.)
+    smalldata = ra[(ra>170.) & (ra<190.) & (dec>0.) & (dec<35.)]
+    n_in = len(smalldata)
+    density = n_in/footprint_area
+    if(n_in==0):
+        density = 1E-6
+    return density
+
+
+def ndens_select(data, source_name, **kwargs):
+
+    """Apply selection function based only on number density and redshift criteria.
+
+    """
+
+    ra = data['RA']
+    dec = data['DEC']
+    z = data['Z']
+    
+    if ('min_z' in data) & ('max_z' in data):
+        in_z = ((z>=kwargs['min_z']) & (z<=kwargs['max_z']))
+    else:
+        in_z = z>0.0
+
+    mock_dens = estimate_density(ra[in_z], dec[in_z])
+    frac_keep = min(kwargs['number_density']/mock_dens , 1.0)
+    if mock_dens < kwargs['number_density']:
+        print("WARNING: mock cannot achieve the goal density for source {} Goal {}. Mock {}".format(source_name, kwargs['number_density'], mock_dens))
+
+
+    n = len(ra)
+    keepornot = np.random.uniform(0.,1.,n)
+    limit = np.zeros(n) + frac_keep
+    kept = keepornot < limit
+    select_sample = (in_z) & (kept)
+
+    target_class = np.zeros(n,dtype=np.int64) - 1
+    target_class[select_sample] = desi_mask.mask(source_name)
+
+    return target_class
+
