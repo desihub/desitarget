47af98029911095ae41a062adfa314c1bda2cc99
Adam Myers <geordiemyers@gmail.com>
2016-11-07 09:09:30 -0800

    removed hack that checked for zero values of dust as sweeps files now fixed

--- py/desitarget/cuts.py
+++ py/desitarget/cuts.py
@@ -382,18 +382,16 @@ def unextinct_fluxes(objects):
     else:
         result = np.zeros(len(objects), dtype=dtype)
 
-#ADM Hack for DR3 because of some corrupt sweeps/Tractor files REMOVE ONCE SWEEPS ARE FIXED!!!
-#    dered_decam_flux = objects['DECAM_FLUX'] / objects['DECAM_MW_TRANSMISSION']
-    dered_decam_flux = np.divide(objects['DECAM_FLUX'] , objects['DECAM_MW_TRANSMISSION'], 
-                                 where=objects['DECAM_MW_TRANSMISSION']!=0)
+    dered_decam_flux = objects['DECAM_FLUX'] / objects['DECAM_MW_TRANSMISSION']
+#    dered_decam_flux = np.divide(objects['DECAM_FLUX'] , objects['DECAM_MW_TRANSMISSION'], 
+#                                 where=objects['DECAM_MW_TRANSMISSION']!=0)
     result['GFLUX'] = dered_decam_flux[..., 1]
     result['RFLUX'] = dered_decam_flux[..., 2]
     result['ZFLUX'] = dered_decam_flux[..., 4]
 
-#ADM Hack for DR3 because of some corrupt sweeps/Tractor files REMOVE ONCE SWEEPS ARE FIXED!!!
-#    dered_wise_flux = objects['WISE_FLUX'] / objects['WISE_MW_TRANSMISSION']
-    dered_wise_flux =  np.divide(objects['WISE_FLUX'] , objects['WISE_MW_TRANSMISSION'],
-                                 where=objects['WISE_MW_TRANSMISSION']!=0)
+    dered_wise_flux = objects['WISE_FLUX'] / objects['WISE_MW_TRANSMISSION']
+#    dered_wise_flux =  np.divide(objects['WISE_FLUX'] , objects['WISE_MW_TRANSMISSION'],
+#                                 where=objects['WISE_MW_TRANSMISSION']!=0)
     result['W1FLUX'] = dered_wise_flux[..., 0]
     result['W2FLUX'] = dered_wise_flux[..., 1]
 
