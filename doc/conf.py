0450f36f03b8ba468b50c2c9adeeff7b8a84288c
Benjamin Alan Weaver <baweaver@lbl.gov>
2017-03-23 14:02:35 -0700

    add more mock modules [skip ci]

--- doc/conf.py
+++ doc/conf.py
@@ -140,7 +140,7 @@ napoleon_include_private_with_doc = True
 autodoc_mock_imports = ['fitsio', 'desisim.io', 'numpy', 'numpy.lib.recfunctions',
                         'astropy.coordinates', 'astropy.io.fits', 'astropy.table',
                         'scipy', 'scipy.optimize', 'matplotlib', 'matplotlib.pyplot',
-                        'matplotlib.patches',
+                        'matplotlib.patches', 'matplotlib.collections',
                         'desispec.brick', 'desispec.io', 'desispec.io.util',
                         'desiutil', 'desiutil.bitmask', 'desiutil.log',
                         'sklearn.ensemble', 'sklearn.externals', 'sklearn.tree']
