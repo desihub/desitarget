430081f3477158309b936a9ad483a6a12425b636
Christophe Yeche <christophe.yeche@cea.fr>
2017-09-01 08:55:01 -0700

    Modify morphology cut for QSO selection for DR5

--- py/desitarget/cuts.py
+++ py/desitarget/cuts.py
@@ -364,7 +364,7 @@ def isQSO_colors(gflux, rflux, zflux, w1flux, w2flux, optical=False):
 
     return qso
 
-def isQSO_cuts(gflux, rflux, zflux, w1flux, w2flux, w1snr, w2snr, deltaChi2,
+def isQSO_cuts(gflux, rflux, zflux, w1flux, w2flux, w1snr, w2snr, deltaChi2, release,
                objtype=None, primary=None):
     """Cuts based QSO target selection
 
@@ -397,7 +397,7 @@ def isQSO_cuts(gflux, rflux, zflux, w1flux, w2flux, w1snr, w2snr, deltaChi2,
     qso &= w1snr > 4
     qso &= w2snr > 2
 
-    qso &= deltaChi2>40.
+    qso &= ((deltaChi2>40.) | (release>=5000) )
 
     if primary is not None:
         qso &= primary
@@ -407,7 +407,7 @@ def isQSO_cuts(gflux, rflux, zflux, w1flux, w2flux, w1snr, w2snr, deltaChi2,
 
     return qso
 
-def isQSO_randomforest(gflux=None, rflux=None, zflux=None, w1flux=None, w2flux=None, objtype=None,
+def isQSO_randomforest(gflux=None, rflux=None, zflux=None, w1flux=None, w2flux=None, objtype=None, release=None,
          deltaChi2=None, primary=None):
     """Target Definition of QSO using a random forest returning a boolean array.
 
@@ -465,6 +465,7 @@ def isQSO_randomforest(gflux=None, rflux=None, zflux=None, w1flux=None, w2flux=N
 
     #define pcut, relaxed cut for faint objects
     pcut = np.where(r>20.0,0.95 - (r-20.0)*0.08,0.95)
+    pcut_DR5 = np.where(r>20.0,0.92 - (r-20.0)*0.10,0.92)
 
     qso = primary.copy()
     qso &= r<rMax
@@ -473,13 +474,13 @@ def isQSO_randomforest(gflux=None, rflux=None, zflux=None, w1flux=None, w2flux=N
     if objtype is not None:
         qso &= _psflike(objtype)
 
-    if deltaChi2 is not None:
-        qso &= deltaChi2>30.
+    if (deltaChi2 is not None) :
+        qso &= ((deltaChi2>30.) | (release>=5000) ) 
 
     if nbEntries==1 : # for call of a single object
         qso &= prob[0]>pcut
     else :
-        qso &= prob>pcut
+        qso &= (((prob>pcut)&(release<5000)) | ((prob>pcut_DR5)&(release>=5000)))
 
     return qso
 
@@ -545,7 +546,8 @@ def _is_row(table):
     else:
         return False
 
-def unextinct_fluxes(objects):
+def unextinct_fluxes(objects
+                     ):
     """
     Calculate unextincted DECam and WISE fluxes
 
@@ -627,6 +629,7 @@ def apply_cuts(objects, qso_selection='randomforest'):
     w1flux = flux['W1FLUX']
     w2flux = flux['W2FLUX']
     objtype = objects['TYPE']
+    release = objects['RELEASE']
 
     gfluxivar = objects['FLUX_IVAR_G']
 
@@ -670,10 +673,10 @@ def apply_cuts(objects, qso_selection='randomforest'):
     if qso_selection=='colorcuts' :
         qso = isQSO_cuts(primary=primary, zflux=zflux, rflux=rflux, gflux=gflux,
                          w1flux=w1flux, w2flux=w2flux, deltaChi2=deltaChi2, objtype=objtype,
-                         w1snr=w1snr, w2snr=w2snr)
+                         w1snr=w1snr, w2snr=w2snr, release=release)
     elif qso_selection == 'randomforest':
         qso = isQSO_randomforest(primary=primary, zflux=zflux, rflux=rflux, gflux=gflux,
-                                 w1flux=w1flux, w2flux=w2flux, deltaChi2=deltaChi2, objtype=objtype)
+                                 w1flux=w1flux, w2flux=w2flux, deltaChi2=deltaChi2, objtype=objtype, release=release)
     else:
         raise ValueError('Unknown qso_selection {}; valid options are {}'.format(qso_selection,
                                                                                  qso_selection_options))
