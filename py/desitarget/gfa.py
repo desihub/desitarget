bcf168edcdf2a9bfc52a5b5bd355a9a3f0f50049
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2018-02-14 07:54:13 -0800

    docstrings + desi logger

--- py/desitarget/gfa.py
+++ py/desitarget/gfa.py
@@ -14,7 +14,29 @@ import desimodel.io
 import glob
 import os
 
+
+
 def near_tile(data, tilera, tiledec, window_ra=4.0, window_dec=4.0):
+    """Trims the input data to a rectangular windonw in RA,DEC.
+    
+    Parameters
+    ----------
+    data : :class:`np.ndarray`
+        Array with target data. Includes at least 'RA' and 'DEC' columns.
+    tilera: :class:`float`
+        Scalar with the central RA coordinate.
+    tiledec: :class:`float`
+        Scalar with the central DEC coordinate
+    window_ra: :class:`float`
+        Value of the window in RA to trim the data.
+    window_dec: :class:`float`
+        Value of the window in DEC to trim the data.
+        
+    Returns
+    -------
+    :class:`bool`
+        Boolean array. True if the target falls inside the window. False otherwise.
+    """
     delta_RA = data['RA'] - tilera
     delta_dec = data['DEC'] - tiledec
     jj = np.fabs(delta_RA) < window_ra
@@ -23,14 +45,35 @@ def near_tile(data, tilera, tiledec, window_ra=4.0, window_dec=4.0):
     jj = jj & (np.fabs(delta_dec) < window_dec)
     return jj
 
-def write_gfa_targets(sweep_dir="./", desi_tiles=None, output_dir="./"):
+def write_gfa_targets(sweep_dir="./", desi_tiles=None, output_path="./", log=None):
+    """Computes and writes to disk GFA targets for every tile
+    
+    Parameters
+    ----------
+    sweep_dir : :class:`string`
+        Path to the sweep files.
+    
+    desi_tiles: :class:`np.ndarray`
+        Set of desitiles to compute the GFA targets. 
+    
+    output_path : :class:`string`
+        Path where the "gfa_targets_tile" files will be written.
+    
+    log : :class: `desiutil.log`
+        Desiutil logger
+    """
+    
+    if log is None:
+        from desiutil.log import get_logger
+        log = get_logger()
+        
     if desi_tiles is None:
         desi_tiles = desimodel.io.load_tiles()
     
     # list sweep files to be used
     sweep_files = desitarget.io.list_sweepfiles(sweep_dir)
     n_sweep = len(sweep_files)
-    print(len(sweep_files), 'sweep files')
+    log.info('{} sweep files'.format(len(sweep_files)))
 
     #load all sweep data
     sweep_data = []
@@ -54,16 +97,16 @@ def write_gfa_targets(sweep_dir="./", desi_tiles=None, output_dir="./"):
 
         sweep_data.append(data)
 
-        print('Loaded file {} out of {}'.format(i, n_sweep))
+        log.info('Loaded file {} out of {}'.format(i, n_sweep))
 
     all_sweep = np.concatenate(sweep_data, axis=0)
 
-    print('There are {:.2f}M targets in the sweeps'.format(len(all_sweep)/1E6))
+    log.info('There are {:.2f}M targets in the sweeps'.format(len(all_sweep)/1E6))
 
     #find IDs of targets on every individual tile
     for i in range(len(desi_tiles)):
         tile_id = desi_tiles['TILEID'][i]
-        print('computing TILEID {:05d} on RA {:6.2f} DEC {:6.2f}'.format(tile_id, desi_tiles['RA'][i], desi_tiles['DEC'][i]))
+        log.info('computing TILEID {:05d} on RA {:6.2f} DEC {:6.2f}'.format(tile_id, desi_tiles['RA'][i], desi_tiles['DEC'][i]))
     
         # select targets in a smaller window centered on tile
         jj = near_tile(all_sweep, desi_tiles['RA'][i], desi_tiles['DEC'][i])
@@ -71,22 +114,41 @@ def write_gfa_targets(sweep_dir="./", desi_tiles=None, output_dir="./"):
         #find GFA targets in the smaller input window
         if np.count_nonzero(jj):
             mini_sweep = all_sweep[jj]
-            print('Inside mini_sweep: {:.2f}M targets'.format(len(mini_sweep)/1E6))
+            log.info('Inside mini_sweep: {:.2f}M targets'.format(len(mini_sweep)/1E6))
         
             targetindices, gfaindices = desimodel.focalplane.on_tile_gfa(tile_id, mini_sweep)
-            print('Found {:d} targets on TILEID {:05d}'.format(len(targetindices), tile_id))
+            log.info('Found {:d} targets on TILEID {:05d}'.format(len(targetindices), tile_id))
 
             if len(targetindices):
                 gfa_targets = np.lib.recfunctions.append_fields(
                     mini_sweep[targetindices], 'GFA_LOC', gfaindices,
                     usemask=False)
 
-                filename = os.path.join(output_dir, "gfa_targets_tile_{:05d}.fits".format(tile_id))        
-                print("writing to {}".format(filename))
+                filename = os.path.join(output_path, "gfa_targets_tile_{:05d}.fits".format(tile_id))        
+                log.info("writing to {}".format(filename))
                 a = fitsio.write(filename, gfa_targets, extname='GFA', clobber=True)
             
 
-def add_gfa_info_to_tiles(gfa_file_path="./", fa_file_path=None, output_path=None):
+def add_gfa_info_to_fa_tiles(gfa_file_path="./", fa_file_path=None, output_path=None, log=None):
+    """Adds GFA info into fiberassign tiles.
+    
+    Parameters
+    ----------
+    gfa_file_path : :class:`string`
+        Path to the "gfa_targets_tile" files.
+    
+    fa_file_path : :class:`string`
+        Path to the results of fiberassign.
+    
+    output_path : :class:`string`
+        Path where the "tile_*" files will be rewritten including the GFA info
+    
+    log : :class: `desiutil.log`
+        Desiutil logger
+    """
+    if log is None:
+        from desiutil.log import get_logger
+        log = get_logger()
     if not os.path.isdir(output_path):
         os.makedirs(output_path, exist_ok=True)
     
@@ -101,10 +163,10 @@ def add_gfa_info_to_tiles(gfa_file_path="./", fa_file_path=None, output_path=Non
         
     if fa_file_path:
         fiberassign_tilefiles = glob.glob(os.path.join(fa_file_path,"tile*.fits"))
-        print(len(fiberassign_tilefiles), 'fiberassign tile files')
+        log.info('{} fiberassign tile files'.format(len(fiberassign_tilefiles)))
     else:
         fiberassign_tilefiles = []
-        print('Empty fiberassign path')
+        log.info('Empty fiberassign path')
         
     fa_tile_id = {}
     for fa_file in fiberassign_tilefiles:
@@ -115,7 +177,7 @@ def add_gfa_info_to_tiles(gfa_file_path="./", fa_file_path=None, output_path=Non
     
     for gfa_id in gfa_tile_id.keys():
         if gfa_id in fa_tile_id.keys():
-            print('rewriting tilefile for tileid {}'.format(gfa_id))
+            log.info('rewriting tilefile for tileid {}'.format(gfa_id))
             gfa_data = fitsio.read(gfa_tile_id[gfa_id])
             fiber_data = fitsio.read(fa_tile_id[gfa_id], ext=1)
             potential_data = fitsio.read(fa_tile_id[gfa_id], ext=2)
