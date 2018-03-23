0df943bebe52c44c625b674c2ad27c5be06ad499
Adam Myers <geordiemyers@gmail.com>
2018-03-23 10:45:41 -0700

    updating unit test files, now that FRACDEV needs to be included

--- py/desitarget/test/make_testdata.py
+++ py/desitarget/test/make_testdata.py
@@ -12,7 +12,9 @@ import numpy as np
 from desitarget.cuts import apply_cuts
 from desitarget.io import read_tractor
 import fitsio
+from time import time
 
+start = time()
 tractordir = '/project/projectdirs/cosmo/data/legacysurvey/dr3.1/tractor/330'
 #tractordir = '/data/legacysurvey/dr3.1/tractor/330/'
 for brick in ['3301m002', '3301m007', '3303p000']:
@@ -25,6 +27,7 @@ for brick in ['3301m002', '3301m007', '3303p000']:
 #    fits.writeto('t/'+basename(filepath), data[keep], header=hdr)
     data, hdr = read_tractor(filepath, header=True)
     fitsio.write('t/'+basename(filepath), data[keep], header=hdr, clobber=True)
+    print('made Tractor file for brick {}...t={:.2f}s'.format(brick,time()-start))
 
 sweepdir = '/project/projectdirs/cosmo/data/legacysurvey/dr3.1/sweep/3.1'
 #sweepdir = '/data/legacysurvey/dr2p/sweep/'
@@ -38,6 +41,7 @@ for radec in ['310m005-320p000', '320m005-330p000', '330m005-340p000']:
 #    fits.writeto('t/'+basename(filepath), data[keep], header=hdr)
     data, hdr = read_tractor(filepath, header=True)
     fitsio.write('t/'+basename(filepath), data[keep], header=hdr, clobber=True)
+    print('made sweeps file for range {}...t={:.2f}s'.format(radec,time()-start))
 
 #ADM adding a file to make a mask for bright stars
 #ADM this should go in its own directory /t2 (others are in t1)
@@ -45,3 +49,4 @@ filepath = '{}/sweep-{}.fits'.format(sweepdir, '190m005-200p000')
 data, hdr = read_tractor(filepath, header=True)
 keep = np.where(data["FLUX_Z"] > 100000)
 fitsio.write('t2/'+basename(filepath), data[keep], header=hdr, clobber=True)
+print('Done...t={:.2f}s'.format(time()-start))
