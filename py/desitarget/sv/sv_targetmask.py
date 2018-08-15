720573681ef22ec48c64004237cbeebf84aa5d74
Adam Myers <geordiemyers@gmail.com>
2018-08-15 13:20:00 -0700

    set up initial SV directory

---
+++ py/desitarget/sv/sv_targetmask.py
@@ -0,0 +1,75 @@
+"""
+desitarget.sv.svtargetmask
+==========================
+
+This looks more like a script than an actual module.
+"""
+from desiutil.bitmask import BitMask
+import yaml
+from pkg_resources import resource_filename
+
+
+_bitdefs = None
+
+
+def _load_bits():
+    """Load bit definitions from yaml file.
+    """
+    global _bitdefs
+    if _bitdefs is None:
+        _filepath = resource_filename('desitarget', "sv/data/svtargetmask.yaml")
+        with open(_filepath) as fx:
+            _bitdefs = yaml.load(fx)
+        try:
+            _load_priorities()
+        except TypeError:
+            pass
+    return
+
+
+def _load_priorities():
+    """Priorities are defined in the yaml file, but they aren't a bitmask
+    and they require some extra processing.
+    """
+    global _bitdefs
+    for maskname, priorities in _bitdefs['priorities'].items():
+        for bitname in priorities:
+            #- "SAME_AS_XXX" enables one bit to inherit priorities from another
+            if isinstance(priorities[bitname], str) and \
+                priorities[bitname].startswith('SAME_AS_'):
+                    other = priorities[bitname][8:]
+                    priorities[bitname] = priorities[other]
+
+            #- fill in default "more" priority to be same as "unobs"
+            if isinstance(priorities[bitname], dict):
+                if 'MORE_ZWARN' not in priorities[bitname]:
+                    priorities[bitname]['MORE_ZWARN'] = priorities[bitname]['UNOBS']
+                if 'MORE_ZGOOD' not in priorities[bitname]:
+                    priorities[bitname]['MORE_ZGOOD'] = priorities[bitname]['UNOBS']
+
+                #- fill in other states as priority=1
+                for state, blat, foo in _bitdefs['sv_obsmask']:
+                    if state not in priorities[bitname]:
+                        priorities[bitname][state] = 1
+            else:
+                priorities[bitname] = dict()
+
+        #- add to the extra info dictionary for this target mask
+        for bitdef in _bitdefs[maskname]:
+            bitname = bitdef[0]
+            bitdef[3]['priorities'] = priorities[bitname]
+    return
+
+#- convert to BitMask objects
+if _bitdefs is None:
+    _load_bits()
+try:
+    sv_desi_mask = BitMask('sv_desi_mask', _bitdefs)
+    sv_mws_mask = BitMask('sv_mws_mask', _bitdefs)
+    sv_bgs_mask = BitMask('sv_bgs_mask', _bitdefs)
+    sv_obsmask = BitMask('sv_obsmask', _bitdefs)
+except TypeError:
+    sv_desi_mask = object()
+    sv_mws_mask = object()
+    sv_bgs_mask = object()
+    sv_obsmask = object()
