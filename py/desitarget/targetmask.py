592c96794027d388a75b31e126760d67c6dabf75
Adam Myers <geordiemyers@gmail.com>
2018-08-23 13:47:21 -0700

    updating mask setting code to make it more portable

--- py/desitarget/targetmask.py
+++ py/desitarget/targetmask.py
@@ -8,35 +8,31 @@ from desiutil.bitmask import BitMask
 import yaml
 from pkg_resources import resource_filename
 
-
-_bitdefs = None
-
-
-def _load_bits():
+def load_mask_bits(prefix=""):
     """Load bit definitions from yaml file.
     """
-    global _bitdefs
-    if _bitdefs is None:
-        _filepath = resource_filename('desitarget', "data/targetmask.yaml")
-        with open(_filepath) as fx:
-            _bitdefs = yaml.load(fx)
+    us = ""
+    if len(prefix) > 0:
+        us = '_'
+    fn = "{}/data/{}{}targetmask.yaml".format(prefix,prefix,us)
+    _filepath = resource_filename('desitarget', fn)
+    with open(_filepath) as fx:
+        bitdefs = yaml.load(fx)
         try:
-            _load_priorities(handle="priorities")
+            bitdefs = _load_mask_priorities(bitdefs,handle="priorities")
         except TypeError:
             pass
         try:
-            _load_priorities(handle="numobs")
+            bitdefs = _load_mask_priorities(bitdefs,handle="numobs")
         except TypeError:
             pass
-    return
+    return bitdefs
 
-
-def _load_priorities(handle="priorities"):
+def _load_mask_priorities(bitdefs,handle="priorities"):
     """Priorities and NUMOBS are defined in the yaml file, but they aren't 
     a bitmask and so require some extra processing.
     """
-    global _bitdefs
-    for maskname, priorities in _bitdefs[handle].items():
+    for maskname, priorities in bitdefs[handle].items():
         for bitname in priorities:
             #- "SAME_AS_XXX" enables one bit to inherit priorities from another
             if isinstance(priorities[bitname], str) and \
@@ -54,21 +50,22 @@ def _load_priorities(handle="priorities"):
                         priorities[bitname]['MORE_ZGOOD'] = priorities[bitname]['UNOBS']
 
                     #- fill in other states as priority=1
-                    for state, blat, foo in _bitdefs['obsmask']:
+                    for state, blat, foo in bitdefs['obsmask']:
                         if state not in priorities[bitname]:
                             priorities[bitname][state] = 1
                 else:
                     priorities[bitname] = dict()
 
         #- add to the extra info dictionary for this target mask
-        for bitdef in _bitdefs[maskname]:
+        for bitdef in bitdefs[maskname]:
             bitname = bitdef[0]
             bitdef[3][handle] = priorities[bitname]
-    return
+    return bitdefs
 
 #- convert to BitMask objects
-if _bitdefs is None:
-    _load_bits()
+#if bitdefs is None:
+#    load_bits()
+_bitdefs = load_mask_bits()
 try:
     desi_mask = BitMask('desi_mask', _bitdefs)
     mws_mask = BitMask('mws_mask', _bitdefs)
