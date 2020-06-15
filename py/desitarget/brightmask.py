d4e17282c83d60237d32b85ba56593a26a7823f6
Adam Myers <geordiemyers@gmail.com>
2020-06-15 12:55:31 -0700

    remove file of unit tests that are completely deprecated

--- py/desitarget/brightmask.py
+++ py/desitarget/brightmask.py
@@ -102,9 +102,9 @@ def get_recent_mask_dir(input_dir=None):
             raise IOError(msg)
         # ADM a fairly exhaustive list of possible mask directories.
         mds = glob(os.path.join(md, "*maglim*")) + \
-              glob(os.path.join(md, "*/*maglim*")) + \
-              glob(os.path.join(md, "*/*/*maglim*"))
-        if len(mds)==0:
+            glob(os.path.join(md, "*/*maglim*")) + \
+            glob(os.path.join(md, "*/*/*maglim*"))
+        if len(mds) == 0:
             msg = "no mask sub-directories found in {}".format(md)
             log.error(msg)
             raise IOError(msg)
