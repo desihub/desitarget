3a73566a4f70e49ee1ae50621243455df6bc4e1a
echaussidon <edmondchau@sfr.fr>
2020-11-19 15:09:43 +0100

    add init files

---
+++ py/desitarget/train/train_test_RF/__init__.py
@@ -0,0 +1,12 @@
+#
+# See top-level LICENSE file for Copyright information
+#
+# -*- coding: utf-8 -*-
+"""
+desitarget
+==========
+
+Tools for RF training QSO targets selection.
+
+.. _DESI: http://desi.lbl.gov
+"""
