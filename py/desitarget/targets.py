1efe0e6afe975a55ed9a5c7a5f254e9661e198b3
Adam Myers <geordiemyers@gmail.com>
2021-05-12 06:19:32 -0700

    bugfix; only apply special QSO numobs reduction in DARK time

--- py/desitarget/targets.py
+++ py/desitarget/targets.py
@@ -571,16 +571,18 @@ def calc_numobs_more(targets, zcat, obscon):
     numobs_more = np.maximum(0, targets['NUMOBS_INIT'] - zcat['NUMOBS'])
 
     if survey == 'main':
-        # ADM If a QSO target is confirmed to have a redshift at
-        # ADM z < midzcut it will need to drop by 2 total observations
-        # ADM (midzcut is defined at the top of this module).
-        ii = targets[desi_target] & desi_mask.QSO != 0
-        for scxname in scnd_mask.names():
-            if scnd_mask[scxname].flavor=="QSO":
-                ii |= targets[scnd_target] & scnd_mask[scxname] != 0
-        ii &= (zcat['ZWARN'] == 0)
-        ii &= (zcat['Z'] < midzcut)
-        numobs_more[ii] = np.maximum(0, numobs_more[ii] - 2)
+        # ADM apply special QSO behavior, but only in dark time.
+        if (obsconditions.mask(obscon) & obsconditions.mask("DARK")) != 0:
+            # ADM If a QSO target is confirmed to have a redshift at
+            # ADM z < midzcut it will need to drop by 2 total observations
+            # ADM (midzcut is defined at the top of this module).
+            ii = targets[desi_target] & desi_mask.QSO != 0
+            for scxname in scnd_mask.names():
+                if scnd_mask[scxname].flavor=="QSO":
+                    ii |= targets[scnd_target] & scnd_mask[scxname] != 0
+            ii &= (zcat['ZWARN'] == 0)
+            ii &= (zcat['Z'] < midzcut)
+            numobs_more[ii] = np.maximum(0, numobs_more[ii] - 2)
 
     return numobs_more
 
