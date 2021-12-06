e07c083296dbbe4d458bcb9b0936786a085c8423
Adam Myers <geordiemyers@gmail.com>
2021-12-05 18:51:10 -0800

    ignore any OVERRIDE ledgers (in a few other places) when re-processing MTLs

--- py/desitarget/mtl.py
+++ py/desitarget/mtl.py
@@ -1467,8 +1467,6 @@ def reprocess_ledger(hpdirname, zcat, obscon="DARK"):
     # ADM find the general format for the ledger files in `hpdirname`.
     # ADM also returning the obsconditions.
     fileform, oc = io.find_mtl_file_format_from_header(hpdirname, returnoc=True)
-    # ADM this is the format for any associated override ledgers.
-    overrideff = io.find_mtl_file_format_from_header(hpdirname, override=True)
 
     # ADM check the obscondition is as expected.
     if obscon != oc:
@@ -1633,16 +1631,8 @@ def reprocess_ledger(hpdirname, zcat, obscon="DARK"):
         # ADM necessary when using io.read_mtl_ledger(unique=True)).
         mtlpix = mtlpix[np.argsort(mtlpix["TARGETID"])]
 
-        # ADM the correct filenames for this pixel number.
+        # ADM the correct filename for this pixel number.
         fn = fileform.format(pix)
-        overfn = overrideff.format(pix)
-
-        # ADM if an override ledger exists, update it and recover its
-        # ADM relevant MTL entries.
-        if os.path.exists(overfn):
-            overmtl = process_overrides(overfn)
-            # ADM add any override entries TO THE END OF THE LEDGER.
-            mtlpix = vstack([mtlpix, overmtl])
 
         # ADM if we're working with .ecsv, simply append to the ledger.
         if ender == 'ecsv':
