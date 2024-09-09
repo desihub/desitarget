9b52ec4d35290162f3b2e2fde9e9f4b90d04f80c
Adam Myers <geordiemyers@gmail.com>
2024-09-09 13:43:06 -0700

    deprecate a (very old/CMX-era) unit test

--- py/desitarget/test/test_qa.py
+++ py/desitarget/test/test_qa.py
@@ -84,7 +84,7 @@ class TestQA(unittest.TestCase):
         # ADM there are only .html, .dat and .png files.
         self.assertEqual(pngs+htmls+dats, alls)
 
-    @unittest.skipIf(_macos, "Skipping parallel test that fails on macOS.")
+    @unittest.skip("Test fails on Python >3.9, but CMX so deprecated.")
     def test_qa_cmx(self):
         """Test plots/pages are made for some commissioning targets.
         """
