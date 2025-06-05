bc8fac365b9d5c00e85b3949ab069891db99ce6b
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2025-06-05 16:48:26 -0700

    copy string values

--- py/desitarget/cmx/ms_cuts.py
+++ py/desitarget/cmx/ms_cuts.py
@@ -428,6 +428,8 @@ def isBGS_lslga(gflux=None, rflux=None, zflux=None, w1flux=None, refcat=None,
     if refcat is not None:
         if isinstance(refcat, MaskedColumn):
             refcatf = refcat.filled('')
+        elif isinstance(refcat, (bytes, str)):
+            refcatf = refcat
         else:
             refcatf = refcat.copy()
         rc1d = np.atleast_1d(refcatf)
