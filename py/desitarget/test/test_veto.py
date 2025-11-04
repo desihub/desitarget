f03b28c99595d04ce17da9f1bfc022ef673d88bc
Adam Myers <geordiemyers@gmail.com>
2025-11-03 21:37:30 -0800

    expand unit tests to have reasonable coverage of end-to-end veto process

--- py/desitarget/test/test_veto.py
+++ py/desitarget/test/test_veto.py
@@ -11,9 +11,9 @@ from uuid import uuid4
 import numpy as np
 
 from desitarget import io
+from desitarget.mtl import process_vetoes
 
-
-class TestIO(unittest.TestCase):
+class TestVETO(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -22,7 +22,11 @@ class TestIO(unittest.TestCase):
     def setUp(self):
         # ADM make an entire copy of the test MTL directory structure.
         self.testdir = "test-{}".format(uuid4().hex)
-        shutil.copytree(self.datadir, self.testdir)
+        shutil.copytree(self.datadir, os.path.join(self.testdir, "main"))
+        # ADM we need to remove the bad test file from the MTL mock
+        # ADM directory, otherwise updates will fail on that file.
+        shutil.rmtree(os.path.join(self.testdir,
+                                   "main", "veto", "bright1b", "bad"))
         self.test_goodfn = os.path.join(
             self.datadir, "veto/bright1b/good/good-veto-bright1b.ecsv")
         self.test_badfn = os.path.join(
@@ -34,7 +38,6 @@ class TestIO(unittest.TestCase):
 
     def test_veto_file(self):
         """Test a good veto file can be read but a bad one can't"""
-
         # ADM check the good data reads as expected.
         data = io.read_mtl_veto_file(self.test_goodfn)
         self.assertTrue(type(data),  astropy.table.table.Table)
@@ -47,6 +50,46 @@ class TestIO(unittest.TestCase):
             badmsg = str(msg)
 
         errorcount = len(badmsg.split(" AND "))
-
+        print('adasdas')
         self.assertEqual(errorcount, 3)
 
+    def test_process_vetoes(self):
+        """Test the end-to-end veto process"""
+        # ADM Process the vetos working in the test directory.
+        process_vetoes("bright1b", survey="main", mtldir=self.testdir)
+
+        amtl1 = io.read_mtl_ledger(os.path.join(self.testdir, "main", "bright1b",
+                                                "mtl-bright1b-hp-661.ecsv"),
+                                                initial=True)
+        amtl2 = io.read_mtl_ledger(os.path.join(self.testdir, "main", "bright1b",
+                                                "mtl-bright1b-hp-704.ecsv"),
+                                                initial=True)
+        bmtl1 = io.read_mtl_ledger(os.path.join(self.testdir, "main", "bright1b",
+                                                "mtl-bright1b-hp-661.ecsv"))
+        bmtl2 = io.read_mtl_ledger(os.path.join(self.testdir, "main", "bright1b",
+                                                "mtl-bright1b-hp-704.ecsv"))
+
+        # ADM test all the initial states...
+        self.assertTrue(np.all(amtl1["TARGET_STATE"]==["M31_GIANT|UNOBS"]))
+        self.assertTrue(np.all(amtl2["TARGET_STATE"]==["M31_GIANT|UNOBS"]))
+        # ADM ...have turned into the vetoed states.
+        self.assertTrue(np.all(bmtl1["TARGET_STATE"]==["VETO|DONE"]))
+        self.assertTrue(np.all(bmtl2["TARGET_STATE"]==["VETO|DONE"]))
+
+        # ADM also check that the final priorities are the DONE priority
+        # ADM and the initial priorities were higher than that.
+        # ADM there are two objects in the first file, so the sum is 4.
+        self.assertTrue(np.sum(amtl1["PRIORITY"]) > 4)
+        self.assertTrue(np.sum(amtl2["PRIORITY"]) > 2)
+        self.assertEqual(np.sum(bmtl1["PRIORITY"]), 4)
+        self.assertEqual(np.sum(bmtl2["PRIORITY"]), 2)
+
+        # ADM also check that no more observations are required
+        # ADM but that more observations were originally required.
+        self.assertTrue(np.all(amtl1["NUMOBS_MORE"] > 0))
+        self.assertTrue(np.sum(amtl2["NUMOBS_MORE"] > 0))
+        self.assertEqual(np.sum(bmtl1["NUMOBS_MORE"]), 0)
+        self.assertEqual(np.sum(bmtl2["NUMOBS_MORE"]), 0)
+
+        # ADM finally, check the mtl-done-vetoes.ecsv file is made.
+        self.assertTrue('mtl-done-vetoes.ecsv' in os.listdir(self.testdir))
