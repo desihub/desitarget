996ad1fc7bdae940c5f1746eb85c2746c5890489
Adam Myers <geordiemyers@gmail.com>
2025-12-18 15:26:10 -0800

    move bitwise negation inside of if/else clause so a deprecation warning is never triggered

--- py/desitarget/cuts.py
+++ py/desitarget/cuts.py
@@ -2085,10 +2085,11 @@ def _prepare_optical_wise(objects, mask=True):
     # ADM flag whether we're using northen (BASS/MZLS) or
     # ADM southern (DECaLS) photometry.
     photsys_north = _isonnorthphotsys(objects["PHOTSYS"])
-    photsys_south = ~photsys_north
-    # ADM catch case where single object or row is passed.
+    # ADM make sure to catch the case of a single object or row.
     if isinstance(photsys_north, bool):
         photsys_south = not(photsys_north)
+    else:
+        photsys_south = ~photsys_north
 
     # ADM the observed r-band flux (used for F standards and MWS, below).
     # ADM make copies of values that we may reassign due to NaNs.
