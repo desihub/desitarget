bdeaf82bb417f3de568cd22634e75b756c481f6c
Adam Myers <geordiemyers@gmail.com>
2026-08-13 15:46:01 -0700

    use minimal tiles-main.ecsv file if DESI_SURVEYOPS environment variable isn't available

--- py/desitarget/test/test_qa.py
+++ py/desitarget/test/test_qa.py
@@ -19,10 +19,13 @@ log = get_logger()
 
 _macos = sys.platform == 'darwin'
 
-if ('DESI_SURVEYOPS' in os.environ) and os.path.exists(os.path.expandvars('$DESI_SURVEYOPS/ops/tiles-main.ecsv')):
-    surveyops_ok = True
-else:
-    surveyops_ok = False
+surveyops_ok = ('DESI_SURVEYOPS' in os.environ) and os.path.exists(
+    os.path.expandvars('$DESI_SURVEYOPS/ops/tiles-main.ecsv'))
+if not surveyops_ok:
+    os.environ["DESI_SURVEYOPS"] = str(
+        resources.files('desitarget').joinpath('test/t'))
+log.info(f'DESI_SURVEYOPS is set to {os.environ["DESI_SURVEYOPS"]}')
+
 
 class TestQA(unittest.TestCase):
 
@@ -63,7 +66,6 @@ class TestQA(unittest.TestCase):
                 if os.path.exists(filename):
                     os.remove(filename)
 
-    @unittest.skipIf(not surveyops_ok, "Skipping QA test that needs $DESI_SURVEYOPS/ops/tiles-main.ecsv")
     @unittest.skipIf(_macos, "Skipping parallel test that fails on macOS.")
     def test_qa_main(self):
         """Test plots/pages made for some main survey target types.
@@ -89,7 +91,6 @@ class TestQA(unittest.TestCase):
         # ADM there are only .html, .dat and .png files.
         self.assertEqual(pngs+htmls+dats, alls)
 
-    @unittest.skipIf(not surveyops_ok, "Skipping QA test that needs $DESI_SURVEYOPS/ops/tiles-main.ecsv")
     @unittest.skipIf(_macos, "Skipping parallel test that fails on macOS.")
     def test_qa_cmx(self):
         """Test plots/pages are made for some commissioning targets.
@@ -104,7 +105,6 @@ class TestQA(unittest.TestCase):
         # ADM there are only .html, .dat and .png files.
         self.assertEqual(pngs+htmls+dats, alls)
 
-    @unittest.skipIf(not surveyops_ok, "Skipping QA test that needs $DESI_SURVEYOPS/ops/tiles-main.ecsv")
     def test_qa_mocks(self):
         """Test mock QA plots/pages
         """
@@ -141,7 +141,6 @@ class TestQA(unittest.TestCase):
         self.assertTrue(set(with_all)-set(no_all) == {'ALL'})
         self.assertTrue(failed)
 
-    @unittest.skipIf(not surveyops_ok, "Skipping _in_desi_footprint test that needs $DESI_SURVEYOPS/ops/tiles-main.ecsv")
     def test_in_footprint(self):
         """Test target class strings are parsed into lists.
         """
